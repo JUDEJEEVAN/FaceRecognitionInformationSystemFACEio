f0894bc2def5ca69677b3221954be789da67fa8c
Jude Jeevan <judejeevan.g1@gmail.com>
2023-06-28 23:18:42 +0530

    This version can encode and save the encoded file using the library pickle

--- encode_generator.py
+++ encode_generator.py
@@ -6,13 +6,18 @@ import os
 # importing the images of people
 folder_path = 'Images'
 path_list = os.listdir(folder_path)
+
+# An array to store the images
 images = []
+
+# this array stores the student IDs
 student_id = []
 for path in path_list:
     images.append(cv2.imread(os.path.join(folder_path, path)))
     student_id.append(os.path.splitext(path)[0])
 
 
+# this function will encode all the images and store them inside another list known as encode list
 def createEncoding(images_list):
     print("Encoding started...")
 
@@ -24,7 +29,14 @@ def createEncoding(images_list):
 
     return encodings_list
 
-
+# passing the list which contain the images in order to record
 known_encode_list = createEncoding(images)
 print("Encoding complete!")
+
+known_encode_list_with_id = [known_encode_list, student_id]
+
+file = open("Encode_file.p", "wb")
+pickle.dump(known_encode_list_with_id, file)
+file.close()
+print("File has been saved!")
 
